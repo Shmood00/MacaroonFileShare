2c76d87bda17e8edb29aecae8f252d873bdc59df
Shmood00 <43577426+Shmood00@users.noreply.github.com>
2020-09-05 14:45:16 -0400

    Update forms.py

--- forms.py
+++ forms.py
@@ -2,18 +2,22 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, MultipleFileField, SelectField
 from wtforms.validators import InputRequired, Email, Length
 
+#Create login form
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=8,max=80)])
 
+#Create register form
 class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=8, max=80)])
 
+#Create file upload form
 class FileUpload(FlaskForm):
     image = MultipleFileField('image', validators=[InputRequired()])
 
+#Create share image form
 class MakeToken(FlaskForm):
     user_email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
   
@@ -21,6 +25,6 @@ class MakeToken(FlaskForm):
 
     expiry_time = SelectField('Expiry Time', validators=[InputRequired()])
 
-
+#Create verify macaroon form
 class VerifyEmail(FlaskForm):
     email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
